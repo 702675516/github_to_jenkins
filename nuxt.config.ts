1e70bb4cd91b38ffd72edf7cd23284b15e88841c
luyuxi <3234898196@qq.com>
2023-07-11 14:36:41 +0800

    添加meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,4 @@
-
-import {defineNuxtConfig}  from "nuxt/config";
+import {defineNuxtConfig} from "nuxt/config";
 
 export default defineNuxtConfig({
     css: [
@@ -8,20 +7,28 @@ export default defineNuxtConfig({
         'normalize.css'
     ],
     app: {
-      head: {
-        title: '',
-        meta: [
-          {
-            name: '',
-            content: 'width=device-width, initial-scale=1, maximum-scale=1',
-          },
-          {
-            name: 'renderer',
-            content: 'webkit',
-          },
-        ],
-        script: [],
-      },
+        head: {
+            title: '',
+            meta: [
+                {
+                    name: '',
+                    content: 'width=device-width, initial-scale=1, maximum-scale=1',
+                },
+                {
+                    name: 'renderer',
+                    content: 'webkit',
+                },
+                {
+                    name: 'description',
+                    content: '星魅生物、美丽、科学、健康、技术团队、全新减脂解决方案、溶脂针、去除多余脂肪、靶向药物、再塑身材'
+                },
+                {
+                    name: 'keywords',
+                    content: '星魅生物、美丽、科学、健康、技术团队、减脂、溶脂针、去除脂肪、再塑身材、减肥'
+                }
+            ],
+            script: [],
+        },
     },
 })
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,5 @@
 <template lang="pug">
 .container
-  Head
-    Meta(name="description" content="星魅生物、美丽、科学、健康、技术团队、全新减脂解决方案、溶脂针、去除多余脂肪、靶向药物、再塑身材")
-    Meta(name="keywords" content="星魅生物、美丽、科学、健康、技术团队、减脂、溶脂针、去除脂肪、再塑身材、减肥")
   aside-nav-menu
   home-page
   middle-page
