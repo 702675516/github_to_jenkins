7ecc5aae93d5ca9487ea3ba0c72bfa60afa61776
jim@MBC <jim@e-kelai.com>
2023-07-11 14:15:34 +0800

    添加了默认webkit渲染元信息;添加了node版本锁定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,5 +5,21 @@ export default defineNuxtConfig({
         '@/assets/main.scss',
         'normalize.css'
     ],
+    app: {
+      head: {
+        title: '',
+        meta: [
+          {
+            name: '',
+            content: 'width=device-width, initial-scale=1, maximum-scale=1',
+          },
+          {
+            name: 'renderer',
+            content: 'webkit',
+          },
+        ],
+        script: [],
+      },
+    },
 })
 
