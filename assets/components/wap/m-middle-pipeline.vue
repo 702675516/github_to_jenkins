de8a7b43625d44520e8bc7977d02bdf7fb9051e6
luyuxi <3234898196@qq.com>
2023-07-17 13:12:39 +0800

    添加地址

--- assets/components/wap/m-middle-pipeline.vue
+++ assets/components/wap/m-middle-pipeline.vue
@@ -10,6 +10,12 @@
     img(src="../../images/wap/230714_星魅网站页面-UPdate-07.jpg")
   .pipeline-m-btm#footer-m
     img(src="../../images/wap/230714_星魅网站页面-UPdate-08.jpg")
+    ul
+      h1 天津星魅生物科技有限公司
+      li 天津自贸试验区(空港经济区)西五道35号汇津广场4号楼4层425号
+      li 电话: 18902106557
+      li email: inform@juvestar.com
+
 </template>
 
 <style scoped lang="scss">
